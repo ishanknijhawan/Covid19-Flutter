08cd199bc77607a8a1a15526c5db1e4050a2b996
ishanknijhawan <ishanknijhawan@gmail.com>
2020-09-03 12:17:13 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/sher_in_the_city/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sher_in_the_city
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/album.dart
@@ -0,0 +1,9 @@
+class Album {
+  int albumId;
+  int id;
+  String title;
+  String url;
+  String thumbnailUrl;
+
+  Album({this.albumId, this.id, this.title, this.url, this.thumbnailUrl});
+}
